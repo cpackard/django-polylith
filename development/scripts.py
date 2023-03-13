5a0f4f02e9fef422319e9b45cf073c16a83d4ead
Christian Packard <6462409+cpackard@users.noreply.github.com>
2023-03-13 15:20:46 -0400

    more create-component improvements

--- development/scripts.py
+++ development/scripts.py
@@ -9,10 +9,19 @@ def create_component() -> None:
     parser.add_argument("name", type=str, help="the name of the new component")
 
     comp_name = parser.parse_args().name
-    comp_src = f"components/{comp_name}/src"
+    comp_src = f"components/{comp_name}/src/{comp_name}"
+    comp_tests = f"components/{comp_name}/src/tests"
 
+    # create parent directories
     subprocess.run(["mkdir", "-p", comp_src])
+    subprocess.run(["mkdir", "-p", comp_tests])
+    # create placeholder files
+    subprocess.run(["touch", f"{comp_tests}/__init__.py", f"{comp_tests}/test_{comp_name}.py"])
+    # Django command to generate boilerplate
     subprocess.run(["poetry", "run", "python", "manage.py", "startapp", comp_name, comp_src])
+    # Cleanup unnecessary modules
+    subprocess.run(["rm", f"{comp_src}/admin.py", f"{comp_src}/tests.py", f"{comp_src}/views.py"])
+    # Poetry cleanup so sub-virtual-envs aren't created
     subprocess.run(
         ["cd", f"components/{comp_name}", "&&", "poetry", "config", "virtualenvs.create", "false", "--local"],
         shell=True,
