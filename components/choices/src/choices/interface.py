f9d15a2143fac8c9b3d0abab295e730579644ccc
Christian Packard <6462409+cpackard@users.noreply.github.com>
2023-03-14 08:33:12 -0400

    add docstrings to interface functions

--- components/choices/src/choices/interface.py
+++ components/choices/src/choices/interface.py
@@ -3,9 +3,10 @@ from . import core
 
 
 def find_choices(question: int) -> list[core.Choice] | None:
-    """TODO: fill"""
+    """Return all choices for the given `question`."""
     return core.find_choices(question)
 
 
 def create_choice(question: int, choice: str) -> core.Choice:
+    """Create a new `choice` associated with `question`."""
     return core.create_choice(question, choice)
